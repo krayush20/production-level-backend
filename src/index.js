dde98b0608570da3b225d96905d8b9bfe614d62a
Rishabh Singh <rishabhsingh@192.168.1.5>
2024-04-26 23:21:54 +0530

    production level backend code without controllers

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import connectDB from './db/index.js';
+import dotenv from "dotenv";
+import { app } from './app.js';
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000)
+})
+.catch((err)=>{
+    console.log("Backend failed!! ", err);
+})
